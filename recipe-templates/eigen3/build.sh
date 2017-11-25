cfb633b45d3c92e007141a84fac82b59a3551084
Nick Hand <nhand@berkeley.edu>
2017-11-25 03:05:47 -0500

    remove eigen3

--- recipe-templates/eigen3/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-source /usr/common/contrib/bccp/conda-channel-bccp/nersc_profile.sh
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release ..
-make install
